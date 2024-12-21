48846ba4855a391d3c37cb6b089bdbd6cef36222
akush1207 <aksh95605@gmail.com>
2024-12-21 11:46:18 +0530

    final commit of "sort colors".

---
+++ src/Array/SortColors.java
@@ -0,0 +1,38 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortColors {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter array length:");
+        int len=sc.nextInt();
+        int[] nums=new int[len];
+        System.out.println("Enter array elements:");
+        for(int i=0;i<len;i++){
+            nums[i]=sc.nextInt();
+        }
+        sort(nums);
+        System.out.println("nums:"+ Arrays.toString(nums));
+    }
+    public static void sort(int[] nums){
+        int low=0,high=nums.length-1,mid=0;
+        while (mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
+        }
+    }
+}
